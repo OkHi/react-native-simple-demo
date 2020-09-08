385391ce8675b0522e7586c20d18826aa1609c4a
Julius Kiano <jgkiano@gmail.com>
2020-09-08 13:57:19 +0300

    added OkVerify.init

--- index.js
+++ index.js
@@ -5,5 +5,14 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import * as OkVerify from '@okhi/react-native-okverify';
+
+OkVerify.init({
+  channelId: 'okhi',
+  channelDescription: 'OkHi verification alerts',
+  channelName: 'OkHi Verification',
+  title: 'Verification in progress',
+  text: 'This will only take a second',
+});
 
 AppRegistry.registerComponent(appName, () => App);
